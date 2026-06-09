dbc258b3c479628abd5ff6db5910f0e1602351e8
Alina Zolotavina <abekmetova@yandex.ru>
2026-06-09 18:40:50 +0400

    fix: year in footer (main page and blog)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,39 @@
 function Footer() {
-    return (
-        <footer className="footer section" id="footer">
-            <div className="footer__column">
-                <p className="footer__paragraph">© 2023 All rights reserved | ZNAC</p>
-                <p className="footer__paragraph">Website design & development by
-                    <a className="footer__link" href="https://www.linkedin.com/in/alina-zolotavina-38a612259/" target="_blank" rel="noreferrer">Alina Zolotavina</a>
-                </p>
-            </div>
-            <div className="footer__column">
-                <a className="footer__link" href="mailto:znacompany@gmail.com">
-                    <div className="footer__icon footer__icon_email"/>
-                    znacompany@gmail.com
-                </a>
-                <a className="footer__link" href="https://www.instagram.com/znac.photo/" target="_blank" rel="noreferrer">
-                    <div className="footer__icon footer__icon_instagram"/>
-                    znac.photo
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer section" id="footer">
+      <div className="footer__column">
+        <p className="footer__paragraph">
+          © {new Date().getFullYear()} All rights reserved | ZNAC
+        </p>
+        <p className="footer__paragraph">
+          Website design & development by
+          <a
+            className="footer__link"
+            href="https://www.linkedin.com/in/alina-zolotavina-38a612259/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alina Zolotavina
+          </a>
+        </p>
+      </div>
+      <div className="footer__column">
+        <a className="footer__link" href="mailto:znacompany@gmail.com">
+          <div className="footer__icon footer__icon_email" />
+          znacompany@gmail.com
+        </a>
+        <a
+          className="footer__link"
+          href="https://www.instagram.com/znac.photo/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="footer__icon footer__icon_instagram" />
+          znac.photo
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
