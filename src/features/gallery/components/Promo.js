525a51cf851099f777148f59adf417201a143693
Alina Zolotavina <abekmetova@yandex.ru>
2026-09-12 01:08:45 +0400

    fix: correct typo in gallery heading

--- src/features/gallery/components/Promo.js
+++ src/features/gallery/components/Promo.js
@@ -3,7 +3,7 @@ function Promo() {
     <section className="promo">
       <div className="promo__text">
         <h1 className="promo__title">
-          A WORLD глан
+          A WORLD
           <br />
           IN FRAMES
         </h1>
